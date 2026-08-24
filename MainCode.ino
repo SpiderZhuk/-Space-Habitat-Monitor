c55c319209a7c4a7f7d6bd179bdf45bd1a38ed2f
SpiderZhuk <rzhukov364@gmail.com>
2026-08-24 23:49:33 +0300

    MainCode.ino

--- MainCode.ino
+++ MainCode.ino
@@ -1,156 +1,131 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2870
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue254;\red0\green0\blue0;
-\red144\green1\blue18;\red19\green118\blue70;\red225\green92\blue84;\red95\green105\blue110;\red17\green134\blue138;
-\red76\green92\blue5;\red35\green22\blue178;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c99608;\cssrgb\c0\c0\c0;
-\cssrgb\c63922\c8235\c8235;\cssrgb\c3529\c52549\c34510;\cssrgb\c91373\c45098\c40000;\cssrgb\c44706\c48627\c50588;\cssrgb\c0\c59216\c61176;
-\cssrgb\c36863\c42745\c1176;\cssrgb\c18824\c18824\c75294;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_BMP085.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Wire.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Adafruit_GFX.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Adafruit_SSD1306.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Adafruit_BMP085.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Adafruit_MPU6050.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 Adafruit_Sensor.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #include\cf0 \strokec4  \cf2 \strokec2 <\cf5 \strokec5 DHT.h\cf2 \strokec2 >\cf0 \cb1 \strokec4 \
-\
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  SCREEN_WIDTH \cf6 \strokec6 128\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  SCREEN_HEIGHT \cf6 \strokec6 64\cf0 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &
-\f1\b \cf7 \strokec7 Wire
-\f0\b0 \cf0 \strokec4 , -\cf6 \strokec6 1\cf0 \strokec4 );\cb1 \
-\
-\cb3 Adafruit_BMP085 bmp;\cb1 \
-\cb3 Adafruit_MPU6050 mpu;\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  DHTPIN \cf6 \strokec6 13\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  DHTTYPE DHT22\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3 DHT dht(DHTPIN, DHTTYPE);\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  BUTTON_PIN \cf6 \strokec6 4\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 #define\cf0 \strokec4  LED_PIN    \cf6 \strokec6 2\cf0 \cb1 \strokec4 \
-\
-\cf2 \cb3 \strokec2 int\cf0 \strokec4  currentScreen = \cf6 \strokec6 0\cf0 \strokec4 ; \cf8 \strokec8 // 0: Temp, 1: Humidity, 2: Pressure, 3: Motion\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 bool\cf0 \strokec4  lastBtnState = \cf9 \strokec9 HIGH\cf0 \strokec4 ;\cb1 \
-\
-\cf2 \cb3 \strokec2 void\cf0 \strokec4  \cf10 \strokec10 setup\cf0 \strokec4 () \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3   
-\f1\b \cf7 \strokec7 Serial
-\f0\b0 \cf0 \strokec4 .\cf7 \strokec7 begin\cf0 \strokec4 (\cf6 \strokec6 115200\cf0 \strokec4 );\cb1 \
-\cb3   \cf7 \strokec7 pinMode\cf0 \strokec4 (BUTTON_PIN, \cf9 \strokec9 INPUT_PULLUP\cf0 \strokec4 );\cb1 \
-\cb3   \cf7 \strokec7 pinMode\cf0 \strokec4 (LED_PIN, \cf9 \strokec9 OUTPUT\cf0 \strokec4 );\cb1 \
-\
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (!display.\cf7 \strokec7 begin\cf0 \strokec4 (SSD1306_SWITCHCAPVCC, \cf11 \strokec11 0x3C\cf0 \strokec4 )) \{\cb1 \
-\cb3     \cf2 \strokec2 for\cf0 \strokec4 (;;);\cb1 \
-\cb3   \}\cb1 \
-\
-\cb3   bmp.\cf7 \strokec7 begin\cf0 \strokec4 ();\cb1 \
-\cb3   mpu.\cf7 \strokec7 begin\cf0 \strokec4 ();\cb1 \
-\cb3   dht.\cf7 \strokec7 begin\cf0 \strokec4 ();\cb1 \
-\
-\cb3   display.clearDisplay();\cb1 \
-\cb3   display.setTextColor(WHITE);\cb1 \
-\cb3 \}\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-\cf2 \cb3 \strokec2 void\cf0 \strokec4  \cf10 \strokec10 loop\cf0 \strokec4 () \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3   \cb1 \
-\cb3   \cf2 \strokec2 bool\cf0 \strokec4  btnState = \cf7 \strokec7 digitalRead\cf0 \strokec4 (BUTTON_PIN);\cb1 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (lastBtnState == \cf9 \strokec9 HIGH\cf0 \strokec4  && btnState == \cf9 \strokec9 LOW\cf0 \strokec4 ) \{\cb1 \
-\cb3     currentScreen = (currentScreen + \cf6 \strokec6 1\cf0 \strokec4 ) % \cf6 \strokec6 4\cf0 \strokec4 ;\cb1 \
-\cb3     \cf7 \strokec7 delay\cf0 \strokec4 (\cf6 \strokec6 50\cf0 \strokec4 );\cb1 \
-\cb3   \}\cb1 \
-\cb3   lastBtnState = btnState;\cb1 \
-\
-\cb3   \cb1 \
-\cb3   \cf2 \strokec2 float\cf0 \strokec4  tempC = bmp.readTemperature();\cb1 \
-\cb3   \cf2 \strokec2 float\cf0 \strokec4  humidity = dht.readHumidity();\cb1 \
-\cb3   \cf2 \strokec2 float\cf0 \strokec4  pressureHPa = bmp.readPressure() / \cf6 \strokec6 100.0\cf0 \strokec4 ;\cb1 \
-\
-\cb3   sensors_event_t a, g, tempMPU;\cb1 \
-\cb3   mpu.getEvent(&a, &g, &tempMPU);\cb1 \
-\
-\cb3   \cf2 \strokec2 float\cf0 \strokec4  accelMag = \cf7 \strokec7 sqrt\cf0 \strokec4 (a.acceleration.x * a.acceleration.x +\cb1 \
-\cb3                         a.acceleration.y * a.acceleration.y +\cb1 \
-\cb3                         a.acceleration.z * a.acceleration.z) / \cf6 \strokec6 9.81\cf0 \strokec4 ;\cb1 \
-\
-\cb3   \cf2 \strokec2 float\cf0 \strokec4  gyroMag = \cf7 \strokec7 sqrt\cf0 \strokec4 (g.gyro.x * g.gyro.x +\cb1 \
-\cb3                        g.gyro.y * g.gyro.y +\cb1 \
-\cb3                        g.gyro.z * g.gyro.z) * (\cf6 \strokec6 180.0\cf0 \strokec4  / \cf6 \strokec6 3.14159\cf0 \strokec4 );\cb1 \
-\
-\cb3  \cb1 \
-\cb3   \cf2 \strokec2 bool\cf0 \strokec4  alert = \cf2 \strokec2 false\cf0 \strokec4 ;\cb1 \
-\
-\cb3   \cf8 \strokec8 // CHANGE PARAMETERS HERE\cf0 \cb1 \strokec4 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (tempC < \cf6 \strokec6 18\cf0 \strokec4  || tempC > \cf6 \strokec6 28\cf0 \strokec4  ) alert = \cf2 \strokec2 true\cf0 \strokec4 ;\cb1 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (humidity < \cf6 \strokec6 25\cf0 \strokec4  || humidity > \cf6 \strokec6 75\cf0 \strokec4  ) alert = \cf2 \strokec2 true\cf0 \strokec4 ;\cb1 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (pressureHPa < \cf6 \strokec6 300\cf0 \strokec4  || pressureHPa > \cf6 \strokec6 1050\cf0 \strokec4 ) alert = \cf2 \strokec2 true\cf0 \strokec4 ;\cb1 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (accelMag < \cf6 \strokec6 0\cf0 \strokec4  || accelMag > \cf6 \strokec6 4\cf0 \strokec4 ) alert = \cf2 \strokec2 true\cf0 \strokec4 ;\cb1 \
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (gyroMag > \cf6 \strokec6 60\cf0 \strokec4 ) alert = \cf2 \strokec2 true\cf0 \strokec4 ;\cb1 \
-\
-\cb3   \cf7 \strokec7 digitalWrite\cf0 \strokec4 (LED_PIN, alert ? \cf9 \strokec9 HIGH\cf0 \strokec4  : \cf9 \strokec9 LOW\cf0 \strokec4 );\cb1 \
-\
-\cb3   \cb1 \
-\cb3   display.clearDisplay();\cb1 \
-\cb3   display.setTextSize(\cf6 \strokec6 1\cf0 \strokec4 );\cb1 \
-\cb3   display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\
-\cb3   \cf2 \strokec2 if\cf0 \strokec4  (alert) \{\cb1 \
-\cb3     display.setCursor(\cf6 \strokec6 85\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3     display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 "[ALERT]"\cf0 \strokec4 );\cb1 \
-\cb3   \}\cb1 \
-\
-\cb3   \cf2 \strokec2 switch\cf0 \strokec4  (currentScreen) \{\cb1 \
-\cb3     \cf2 \strokec2 case\cf0 \strokec4  \cf6 \strokec6 0\cf0 \strokec4 : \cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 "--- TEMPERATURE ---"\cf0 \strokec4 );\cb1 \
-\cb3       display.setTextSize(\cf6 \strokec6 2\cf0 \strokec4 );\cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 10\cf0 \strokec4 , \cf6 \strokec6 25\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (tempC, \cf6 \strokec6 1\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 " C"\cf0 \strokec4 );\cb1 \
-\cb3       \cf2 \strokec2 break\cf0 \strokec4 ;\cb1 \
-\
-\cb3     \cf2 \strokec2 case\cf0 \strokec4  \cf6 \strokec6 1\cf0 \strokec4 : \cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 "--- HUMIDITY ---"\cf0 \strokec4 );\cb1 \
-\cb3       display.setTextSize(\cf6 \strokec6 2\cf0 \strokec4 );\cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 10\cf0 \strokec4 , \cf6 \strokec6 25\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (humidity, \cf6 \strokec6 1\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 " %"\cf0 \strokec4 );\cb1 \
-\cb3       \cf2 \strokec2 break\cf0 \strokec4 ;\cb1 \
-\
-\cb3     \cf2 \strokec2 case\cf0 \strokec4  \cf6 \strokec6 2\cf0 \strokec4 : \cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 "--- PRESSURE ---"\cf0 \strokec4 );\cb1 \
-\cb3       display.setTextSize(\cf6 \strokec6 2\cf0 \strokec4 );\cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 25\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (pressureHPa, \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 " hPa"\cf0 \strokec4 );\cb1 \
-\cb3       \cf2 \strokec2 break\cf0 \strokec4 ;\cb1 \
-\
-\cb3     \cf2 \strokec2 case\cf0 \strokec4  \cf6 \strokec6 3\cf0 \strokec4 : \cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 0\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 "--- MOTION ---"\cf0 \strokec4 );\cb1 \
-\cb3       display.setTextSize(\cf6 \strokec6 1\cf0 \strokec4 );\cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 20\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 "Accel: "\cf0 \strokec4 ); display.\cf7 \strokec7 print\cf0 \strokec4 (accelMag, \cf6 \strokec6 2\cf0 \strokec4 ); display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 " g"\cf0 \strokec4 );\cb1 \
-\cb3       display.setCursor(\cf6 \strokec6 0\cf0 \strokec4 , \cf6 \strokec6 40\cf0 \strokec4 );\cb1 \
-\cb3       display.\cf7 \strokec7 print\cf0 \strokec4 (\cf5 \strokec5 "Gyro:  "\cf0 \strokec4 ); display.\cf7 \strokec7 print\cf0 \strokec4 (gyroMag, \cf6 \strokec6 1\cf0 \strokec4 ); display.\cf7 \strokec7 println\cf0 \strokec4 (\cf5 \strokec5 " deg/s"\cf0 \strokec4 );\cb1 \
-\cb3       \cf2 \strokec2 break\cf0 \strokec4 ;\cb1 \
-\cb3   \}\cb1 \
-\
-\cb3   display.display();\cb1 \
-\cb3   \cf7 \strokec7 delay\cf0 \strokec4 (\cf6 \strokec6 50\cf0 \strokec4 );\cb1 \
-\cb3 \}\cb1 \
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+Adafruit_BMP085 bmp;
+Adafruit_MPU6050 mpu;
+
+#define DHTPIN 13
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+#define BUTTON_PIN 4
+#define LED_PIN    2
+
+int currentScreen = 0; // 0: Temp, 1: Humidity, 2: Pressure, 3: Motion
+bool lastBtnState = HIGH;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for(;;);
+  }
+
+  bmp.begin();
+  mpu.begin();
+  dht.begin();
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+  
+  bool btnState = digitalRead(BUTTON_PIN);
+  if (lastBtnState == HIGH && btnState == LOW) {
+    currentScreen = (currentScreen + 1) % 4;
+    delay(50);
+  }
+  lastBtnState = btnState;
+
+  
+  float tempC = bmp.readTemperature();
+  float humidity = dht.readHumidity();
+  float pressureHPa = bmp.readPressure() / 100.0;
+
+  sensors_event_t a, g, tempMPU;
+  mpu.getEvent(&a, &g, &tempMPU);
+
+  float accelMag = sqrt(a.acceleration.x * a.acceleration.x +
+                        a.acceleration.y * a.acceleration.y +
+                        a.acceleration.z * a.acceleration.z) / 9.81;
+
+  float gyroMag = sqrt(g.gyro.x * g.gyro.x +
+                       g.gyro.y * g.gyro.y +
+                       g.gyro.z * g.gyro.z) * (180.0 / 3.14159);
+
+ 
+  bool alert = false;
+
+  // CHANGE PARAMETERS HERE
+  if (tempC < 18 || tempC > 28 ) alert = true;
+  if (humidity < 25 || humidity > 75 ) alert = true;
+  if (pressureHPa < 300 || pressureHPa > 1050) alert = true;
+  if (accelMag < 0 || accelMag > 4) alert = true;
+  if (gyroMag > 60) alert = true;
+
+  digitalWrite(LED_PIN, alert ? HIGH : LOW);
+
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+
+  if (alert) {
+    display.setCursor(85, 0);
+    display.print("[ALERT]");
+  }
+
+  switch (currentScreen) {
+    case 0: 
+      display.setCursor(0, 0);
+      display.println("--- TEMPERATURE ---");
+      display.setTextSize(2);
+      display.setCursor(10, 25);
+      display.print(tempC, 1);
+      display.print(" C");
+      break;
+
+    case 1: 
+      display.setCursor(0, 0);
+      display.println("--- HUMIDITY ---");
+      display.setTextSize(2);
+      display.setCursor(10, 25);
+      display.print(humidity, 1);
+      display.print(" %");
+      break;
+
+    case 2: 
+      display.setCursor(0, 0);
+      display.println("--- PRESSURE ---");
+      display.setTextSize(2);
+      display.setCursor(0, 25);
+      display.print(pressureHPa, 0);
+      display.print(" hPa");
+      break;
+
+    case 3: 
+      display.setCursor(0, 0);
+      display.println("--- MOTION ---");
+      display.setTextSize(1);
+      display.setCursor(0, 20);
+      display.print("Accel: "); display.print(accelMag, 2); display.println(" g");
+      display.setCursor(0, 40);
+      display.print("Gyro:  "); display.print(gyroMag, 1); display.println(" deg/s");
+      break;
+  }
+
+  display.display();
+  delay(50);
 }
